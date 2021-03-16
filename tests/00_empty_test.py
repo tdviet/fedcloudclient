6a069916dffac6e7deccf11319df97985ada6fa6
tdviet <tdviet@gmail.com>
2021-03-16 13:27:55 +0100

    Add empty test

---
+++ tests/00_empty_test.py
@@ -0,0 +1,8 @@
+def test_empty():
+    """
+    PyTest tests are callables whose names start with "test"
+    (by default)
+    It looks for them in modules whose name contains "test"
+    (by default)
+    """
+    pass
