b14594642e84c36d4ed3c720d14f43f044aed569
jaro221 <jaro15926@gmail.com>
2025-05-15 13:09:36 +0200

    wfwfwfwfaaaaaa

--- docs/conf.py
+++ docs/conf.py
@@ -13,12 +13,7 @@ author = 'jaro221'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',    # for automodule, autoclass, etc.
-    'sphinx.ext.napoleon',   # if you use Google/NumPy-style docstrings
-    'sphinx.ext.viewcode',   # optional: add links to highlighted source
-    # any other extensions you need…
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -29,5 +24,5 @@ root_doc = 'rename'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
--- docs/rename.rst
+++ docs/rename.rst
@@ -1,5 +1,5 @@
 .. fedcloudclient documentation master file, created by
-   sphinx-quickstart on Thu May 15 11:19:14 2025.
+   sphinx-quickstart on Thu May 15 13:09:10 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
