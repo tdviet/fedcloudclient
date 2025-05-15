77538134f39f7024eb1ad9962460d0e366a63fdb
jaro221 <jaro15926@gmail.com>
2025-05-15 13:31:55 +0200

    13_31_hod

--- docs/conf.py
+++ docs/conf.py
@@ -13,16 +13,20 @@ author = 'jaro221'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',    # for automodule, autoclass, etc.
+    'sphinx.ext.napoleon',   # if you use Google/NumPy-style docstrings
+    'sphinx.ext.viewcode',   # optional: add links to highlighted source
+    # any other extensions you need…
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 root_doc = 'rename'
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
