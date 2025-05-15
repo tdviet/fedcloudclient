7314c83b05a4bd15c53ad1bbe77733c3c93439f9
jaro221 <jaro15926@gmail.com>
2025-05-15 14:45:40 +0200

    aaqwrqwr

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-root_doc = 'rename'
+root_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
